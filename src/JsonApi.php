4fb709a99b063770a2a4ecabdd962bfea6c7c0e7
Rich Gerdes <rich@richgerdes.com>
2024-01-10 23:53:09 -0500

    Add JsonApi class for constants.

---
+++ src/JsonApi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RichGerdes\JsonApi;
+
+class JsonApi {
+
+  public const KEYWORD_ATTRIBUTES = 'attributes';
+
+  public const KEYWORD_DATA = 'data';
+
+  public const KEYWORD_ID = 'id';
+
+  public const KEYWORD_INCLUDED = 'included';
+
+  public const KEYWORD_JSONAPI = 'jsonapi';
+
+  public const KEYWORD_JSONAPI_VERSION = 'version';
+
+  public const KEYWORD_JSONAPI_EXT = 'ext';
+
+  public const KEYWORD_JSONAPI_PROFILE = 'profile';
+
+  public const KEYWORD_JSONAPI_META = static::KEYWORD_META;
+
+  public const KEYWORD_RELATED = 'related';
+
+  public const KEYWORD_RELATIONSHIPS = 'relationships';
+
+  public const KEYWORD_SELF = 'self';
+
+  public const KEYWORD_TYPE = 'type';
+
+}
